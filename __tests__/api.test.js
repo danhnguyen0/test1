9b64a330e1aeecfd130db1aa25e5dabc39a8008f
danhnguyen0 <danhjustin147@gmail.com>
2025-11-18 00:32:48 -0600

    fix test

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,14 +1,33 @@
+jest.mock("next", () => {
+  return () => ({
+    prepare: () => Promise.resolve(),
+    getRequestHandler: () => {
+      return (req, res) => {
+        res.statusCode = 200;
+        res.end("OK");
+      };
+    }
+  });
+});
+
 const request = require("supertest");
+const http = require("http");
 const createServer = require("../createServer");
 
-let server;
+let app;
+let httpServer;
 
 beforeAll(async () => {
-  server = await createServer();
+  app = await createServer();
+  httpServer = http.createServer(app).listen();   // start listening
+});
+
+afterAll((done) => {
+  httpServer.close(done);
 });
 
 test("GET /api/hello returns JSON message", async () => {
-  const res = await request(server).get("/api/hello");
+  const res = await request(httpServer).get("/api/hello");
   expect(res.statusCode).toBe(200);
   expect(res.body).toEqual({ message: "Hello from Express API!" });
 });
